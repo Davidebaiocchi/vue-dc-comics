6e22c179e4a42b159282fb8b60874ad990f28214
davide <davide.baiocchi97@gmail.com>
2021-07-06 14:07:32 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    border: 0;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="hello">
-    
+  <div class="container">
+    <img src="@/assets/dc-logo.png" alt="Logo Dc">
+    <ul> 
+      <li>characters</li>
+      <li>comics</li>
+      <li>movies</li>
+      <li>tv</li>
+      <li>games</li>
+      <li>collectibles</li>
+      <li>videos</li>
+      <li>fans</li>
+      <li>news</li>
+      <li>shop</li>
+    </ul>
   </div>
 </template>
 
@@ -13,5 +25,35 @@ export default {
 
 
 <style scoped>
+  .container {
+    height: 100px;
+    line-height: 100px;
+  }
 
+  img {
+    height: 100%;
+    padding: 10px 0;
+    margin-left: 300px;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    right: 250px;
+  }
+
+  li {
+    text-transform: uppercase;
+    padding: 0 15px;
+    cursor: pointer;
+    font-weight: 600;
+    color: rgb(75, 75, 75);
+  }
+
+  li:hover {
+    color: rgb(0, 132, 255);
+  }
 </style>
